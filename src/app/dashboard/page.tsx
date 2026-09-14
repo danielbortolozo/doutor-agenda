434fa1b55f53594e40c33b7d09fd51c2c2fb23f4
unknown <danielbortolozo@hotmail.com>
2026-09-14 02:36:28 -0300

    feat: add clinic form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 
 
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -18,6 +21,14 @@ const DashboardPage = async () => {
         redirect("/authentication");
     }   
 
+    const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(usersToClinicsTable.userId, session?.user.id),
+    });
+
+
+    if (clinics.length === 0) {
+        redirect("/clinic-form");
+    }
 
     return (    
         <div className="className">
